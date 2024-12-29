d4fe2d048675ed293a80cbf257cea54c3b46b332
cindypuzzle <cindylite@126.com>
2024-12-29 16:46:29 +0800

    游戏改名

--- routes/sliding_puzzle.js
+++ routes/sliding_puzzle.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('sliding_puzzle', { title: '数字华容道' });
+    res.render('sliding_puzzle', { title: '一举两得' });
 });
 
 module.exports = router; 
